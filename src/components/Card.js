46ad138316b58491270daa954962f9c83557f080
Prasad Surase <prasad.surase@coupa.com>
2022-04-07 18:34:39 +0530

    Added Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
 import './Card.css';
 
 function Card(props) {
+  const classes = "card " + props.className;
   return (
-    <div className="card">
+    <div className={classes}>
       {props.children}
     </div>
   );
