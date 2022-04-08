836345147c67916992ae8e4a650719ab0480faf9
Prasad Surase <prasad.surase@coupa.com>
2022-04-08 19:58:28 +0530

    Added form component

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,24 @@
+import './ExpenseForm.css';
+
+const ExpenseForm = (props) => {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" />
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default ExpenseForm;
