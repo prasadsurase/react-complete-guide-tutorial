d89bcc94cfe14c7d2b5b9b30c09953339cacce69
Prasad Surase <prasad.surase@coupa.com>
2022-04-08 21:02:44 +0530

    Used useState for form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,37 @@
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+
+  const enteredTitleHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const enteredDateHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const enteredAmountHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={enteredTitleHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" />
+          <input type="date" onChange={enteredDateHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" />
+          <input type="number" onChange={enteredAmountHandler} />
         </div>
       </div>
     </form>
