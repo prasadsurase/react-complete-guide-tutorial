c4d9a2001d2dbd43b7a54ff39e7492d54f85f9cd
Prasad Surase <prasad.surase@coupa.com>
2022-04-08 22:16:32 +0530

    Added year filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,25 @@
+import React, { useState } from 'react';
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from './../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
   const expenses = props.expenses;
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear)
+  }
 
   return (
-    <div className="expenses">
+    <Card className="expenses">
+      <ExpensesFilter selectedYear={filteredYear} onChangeFilter={filterChangeHandler}/>
       <ExpenseItem key={expenses[0].id} title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
       <ExpenseItem key={expenses[1].id} title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       <ExpenseItem key={expenses[2].id} title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
       <ExpenseItem key={expenses[3].id} title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
-    </div>
+    </Card>
   );
 };
 
